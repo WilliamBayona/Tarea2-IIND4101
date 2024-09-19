036e41b353d2a79928b5968036ea63e2bc1ab2e8
Marti820 <j.vasquezc@uniandes.edu.co>
2024-09-18 22:27:06 -0500

    aaaaaaa

--- Punto1.py
+++ Punto1.py
@@ -7,17 +7,92 @@ book = load_workbook("Tarea 2-202420.xlsx")
 Sheet=book["Opti-drones"]
 
 #Definicion de Parametros
-b = {} #Beneficios
-for fila in range(2,27):
+L = [] #Lugares
+for fila in range(2,28):
     lugar = Sheet.cell(fila,1).value
-    print(lugar)
+    #print(lugar)
+    L.append(lugar)
     #for columna in range (2,8):
         
       #  calle = Sheet.cell(1,columna).value
        # b[(lugar,calle)]=Sheet.cell(fila,columna).value
 
-print(b)
+print(L)
+
+#Definicion de Parametros
+A = [] #arcos posibles
+for fila1 in range(2,28):
+    lugar1 = Sheet.cell(fila1,1).value
+    for fila2 in range(2,28):
+    #print(lugar)
+        lugar2 = Sheet.cell(fila2,1).value
+        if lugar1 != lugar2:
+            lugar = (lugar1, lugar2)
+            A.append(lugar)
+    #for columna in range (2,8):
         
+      #  calle = Sheet.cell(1,columna).value
+       # b[(lugar,calle)]=Sheet.cell(fila,columna).value
+
+print(A)
+
+#Parametros
+
+#Definicion de Parametros
+X = {} #posicion x
+for fila in range(2,28):
+    lugar =  Sheet.cell(fila,1).value
+    posX =  Sheet.cell(fila,2).value
+    X[lugar] = posX
+print(X)
+
+Y = {} #posicion y
+for fila in range(2,28):
+    lugar =  Sheet.cell(fila,1).value
+    posY =  Sheet.cell(fila,3).value
+    Y[lugar] = posY
+print(Y)
+
+F = {}
+for fila in range(2,28):
+    lugar =  Sheet.cell(fila,1).value
+    posY =  Sheet.cell(fila,4).value
+    F[lugar] = posY
+print(F)
+
+M = {}
+for i in L:
+    for j in L:
+        if i != j: 
+            M[(i,j)] = abs(X[i]-X[j])+abs(Y[i]-Y[j])
+print(M)
+
+
+#Modelo de Optimización
+m = Model("Tarea2")
+
+#Definicion de variables de decisión
+x = m.addVars(A,vtype=GRB.BINARY, name = "x")
+
+#Restricciones del problema
+#1
+#for f in F:
+#    m.addConstr(quicksum(x[i,j] for s in S) == 1)
+#2
+m.addConstr(quicksum(x['Hangar',i] for i in L if i != 'Hangar') == 5)
+m.addConstr(quicksum(x[i,'Hangar'] for i in L if i != 'Hangar') == 5)
+#3
+for i in L :
+    for j in L :
+        if i !=j: 
+            m.addConstr(x[i,j] + x[j,i] <= 1)
+
+#F.O
+m.setObjective(quicksum(x[a]*M[a] for a in A),GRB.MAXIMIZE)
+m.update()
+#m.setParam("Outputflag",0)
+m.optimize()
+
 """
 #Conjuntos
 S = list() #Sedes
